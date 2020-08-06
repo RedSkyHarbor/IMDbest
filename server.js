147272b9dd68b144a7ae646a2d7f93f18f6bbd6c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-06 16:35:35 -0400

    read port from env variable when in prod

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const path = require('path')
+
+const port = process.env.PORT || 5000
 const app = express()
-const port = 5000
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('/hello', (req, res) => {
   res.send({ 'hello':'Hello World!'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Express server is listening on port ${port}`);
 })

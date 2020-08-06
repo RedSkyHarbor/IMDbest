823df931e526635a9108d093d17fddc965c1768c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-06 16:23:30 -0400

    display hello world response

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    res: [],
+    test: 'test'
+  }
+
+  componentDidMount() {
+    this.query()
+  }
+
+  query = () => {
+    // Get hello world
+    fetch('/hello')
+      .then(res => res.json())
+      .then(json => this.setState({ res: json['hello'] }))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { res } = this.state
+    const { test } = this.state
+    return (
+      <div>
+        <div>{res}</div>
+        <div>{test}</div>
+      </div>
+    )
+  }
 }
 
 export default App;

a18721562752b1409145956186cc7284d66e296f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-06 21:12:30 -0400

    Read local credentials from string

---
+++ server-config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production';
+
+console.log('is this production? ', isProduction);
+ 
+const connectionString = 'postgresql://jessebreuer-penello@localhost:5432/imdbest';
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: false
+});
+
+module.exports = { pool };

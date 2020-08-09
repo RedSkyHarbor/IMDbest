e12b0ad436cbbc7df6c6fddedc87ed60b05c33e4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-09 16:20:45 -0400

    add input validation to postMovie

--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,6 +1,5 @@
+const { Validator } = require('node-input-validator');
 const { get_movies, get_movie, search_movie, create_movie } = require('../services/movies');
-
-// TODO input validation with JOI
 
 /* gets all movies */
 const getMovies = async (req, res) => {
@@ -28,6 +27,26 @@ const searchMovie = async (req, res) => {
 /* create a new movie */
 const postMovie = async (req, res) => {
     // TODO should require authentication, only admins can do this
+
+    const validator = new Validator(req.body, {
+        title: 'required|string|maxLength:128',
+        slug: 'required|string|maxLength:128',
+        genres: 'required|string|maxLength:64',
+        release_date: 'required|string|minLength:10|maxLength:64',
+        length: 'required|string|minLength:2|maxLength:64',
+        fcc_rating: 'required|string|maxLength:5',
+        picture_url: 'required|string|maxLength:255',
+        summary: 'required|string|maxLength:511'
+    });
+
+    const matched = await validator.check();
+
+    if (!matched) {
+        res.status(400).send(validator.errors);
+        return;
+    }
+
+
     let result = {}
     try {
         const { title, slug, genres, release_date, length, fcc_rating, picture_url, summary } = await req.body;

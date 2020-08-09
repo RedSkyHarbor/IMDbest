a3a2a0401aa86e9c7ba6ac8d3013926bc6abf83c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-09 16:02:00 -0400

    add input validation to user registration

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -1,6 +1,22 @@
+const { Validator } = require('node-input-validator');
 const { registration } = require('../services/sessions');
 
 const postRegistration = async (req, res) => {
+
+    const validator = new Validator(req.body, {
+        username: 'required|minLength:3|maxLength:31',
+        password: 'required|minLength:5|maxLength:256',
+        email: 'required|email|maxLength:64',
+        is_admin: 'required|boolean'
+    });
+
+    const matched = await validator.check();
+
+    if (!matched) {
+        res.status(400).send(validator.errors);
+        return;
+    }
+
     let result = {};
     try {
         let { username, password, email, is_admin } = await req.body;

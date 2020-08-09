6d841372c43716046a58524387924f364057fed3
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-09 15:47:08 -0400

    remove joi dependency

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -1,9 +1,7 @@
 const { registration } = require('../services/sessions');
 
 const postRegistration = async (req, res) => {
-    // TODO input validation with NPM package JOI
-    // if fails validation status code 400 Bad Request
-    let result = {}
+    let result = {};
     try {
         let { username, password, email, is_admin } = await req.body;
         result.response = await registration(username, password, email, is_admin);

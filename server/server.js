ed891b250b9e7a2f0657d8ad55f2fa10b3680579
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-13 17:50:52 -0400

    eslint formatting

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 // Require routes
-const index = require('./routes/index');
-const movies = require('./routes/movies');
-const sessions = require('./routes/sessions');
-const ratings = require('./routes/ratings');
+const index = require("./routes/index");
+const movies = require("./routes/movies");
+const sessions = require("./routes/sessions");
+const ratings = require("./routes/ratings");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -13,14 +13,14 @@ const app = express();
 app.use(express.json());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //Routes
-app.use('/', index);
-app.use('/api/movies', movies);
-app.use('/api/sessions', sessions);
-app.use('/api/ratings', ratings);
+app.use("/", index);
+app.use("/api/movies", movies);
+app.use("/api/sessions", sessions);
+app.use("/api/ratings", ratings);
 
 app.listen(port, () => {
-	console.log(`Express server is listening on port ${port}`);
-})
+  console.log(`Express server is listening on port ${port}`);
+});

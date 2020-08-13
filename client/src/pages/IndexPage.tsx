9ec3ddcc68f56c25e8f65051b488f21212faf4fb
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-13 17:35:48 -0400

    created component for the indexPage

---
+++ client/src/pages/IndexPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MovieCards } from "../components/MovieCards";
+
+export const IndexPage: React.FC = () => {
+  return (
+    <div>
+      <MovieCards />
+    </div>
+  );
+};

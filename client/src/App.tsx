c5aeb520a100d122d45b9209cc1e5acf6adb9a21
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-13 17:35:31 -0400

    app component's sole responsibility is for routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,12 @@
-import React, { useState, useEffect } from "react";
-
-interface movie {
-  id: number;
-  title: string;
-  slug: string;
-  genres: string;
-  release_date: string;
-  length: string;
-  fcc_rating: string;
-  picture_url: string;
-  summary: string;
-}
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { IndexPage } from "./pages/IndexPage";
 
 const App: React.FC = () => {
-  const [movies, setMovies] = useState<movie[]>([]);
-
-  useEffect(() => {
-    fetch("/api/movies", {
-      method: "GET",
-    })
-      .then((res) => res.json())
-      .then((json) => setMovies(json))
-      .catch((err) => console.error(err));
-  });
-
   return (
-    <section>
-      {movies.map((movie, index) => (
-        <div key={index}>
-          <p>{movie.id}</p>
-          <p>{movie.title}</p>
-          <p>{movie.slug}</p>
-          <p>{movie.genres}</p>
-          <p>{movie.release_date}</p>
-          <p>{movie.length}</p>
-          <p>{movie.fcc_rating}</p>
-          <img src={movie.picture_url} alt="movie poster"></img>
-          <p>{movie.summary}</p>
-          <hr />
-        </div>
-      ))}
-    </section>
+    <Router>
+      <Route path="/" exact component={IndexPage} />
+    </Router>
   );
 };
 

93010e90b8b5d5950549d202f1cbb7d6accc2a73
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-13 18:07:57 -0400

    refactor from eslint

--- server/services/index.js
+++ server/services/index.js
@@ -1,14 +1,16 @@
-const { pool } = require('./database-config');
+const { pool } = require("./database-config");
 
 async function get_movie_card_data() {
-    try {
-        const results = await pool.query('SELECT title, picture_url, avg(rating) FROM movies INNER JOIN ratings on movies.id = ratings.movieId GROUP BY title, picture_url');
-        return results.rows;
-    } catch (e) {
-        return [];
-    }
+  try {
+    const results = await pool.query(
+      "SELECT title, picture_url, avg(rating) FROM movies INNER JOIN ratings on movies.id = ratings.movieId GROUP BY title, picture_url"
+    );
+    return results.rows;
+  } catch (e) {
+    return [];
+  }
 }
 
 module.exports = {
-    get_movie_card_data
-}
+  get_movie_card_data,
+};

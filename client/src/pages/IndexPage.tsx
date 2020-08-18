7bc5e4cdddd605975e9f2f68a993f45a4f279bfa
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-18 18:05:04 -0400

    leave movie ID in local storage

--- client/src/pages/IndexPage.tsx
+++ client/src/pages/IndexPage.tsx
@@ -3,7 +3,6 @@ import { MovieCards } from "../components/movies/MovieCards";
 import { Header } from "../components/header/Header";
 
 export const IndexPage: React.FC = () => {
-  localStorage.removeItem("movie_id"); // Clean up
   return (
     <div>
       <Header />

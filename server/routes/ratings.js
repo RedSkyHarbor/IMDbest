3c4aee8c14ab2340a864205d1e40024e893bc073
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-20 15:29:41 -0400

    eslint refactor

--- server/routes/ratings.js
+++ server/routes/ratings.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getRatings, getRating, postRating, updateRating } = require('../controllers/ratings');
+const {
+  getRatings,
+  getRating,
+  postRating,
+  updateRating,
+} = require("../controllers/ratings");
 
-router.get('/:movieId', getRatings);
-router.get('/:movieId/:userId', getRating);
-router.post('/', postRating);
-router.put('/:movieId/:userId', updateRating);
+router.get("/:movieId", getRatings);
+router.get("/:movieId/:userId", getRating);
+router.post("/", postRating);
+router.put("/:movieId/:userId", updateRating);
 
 module.exports = router;

823d860a4306feccd4b28d6195f72bb3a9de650b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-20 17:22:57 -0400

    clear localstorage of all garbage after logout

--- client/src/components/registration/Logout.tsx
+++ client/src/components/registration/Logout.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-const handleLogout = () => {
-  localStorage.removeItem("auth-token");
-};
+import { Link, useHistory } from "react-router-dom";
 
 export const Logout: React.FC = () => {
+  let history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem("auth-token");
+    localStorage.removeItem("movie_id");
+    history.push("/");
+  };
+
   return (
     <Link to="/" onClick={handleLogout}>
       Log out

3b3997221860bda03a811ae2927dc2b31ce3d137
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-21 16:31:27 -0400

    Ask user to log in before leaving a comment

--- client/src/pages/MoviePage.tsx
+++ client/src/pages/MoviePage.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { MovieDetails } from "../components/movies/MovieDetails";
 import { Header } from "../components/header/Header";
+import { MovieComments } from "../components/movies/MovieComments";
+import { CommentForm } from "../components/movies/forms/CommentForm";
+import { Login } from "../components/registration/Login";
 
 export const MoviePage: React.FC = () => {
   return (
     <div>
       <Header />
       <MovieDetails />
+      {localStorage.getItem("auth-token") ? (
+        <CommentForm />
+      ) : (
+        <p>
+          <Login /> to leave a comment
+        </p>
+      )}
+      <MovieComments />
     </div>
   );
 };

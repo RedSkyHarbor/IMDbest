ccf3b5396603fb7bdbf8c653b931061c532ceb18
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-22 20:29:36 -0400

    add input validation

--- client/src/components/movies/forms/CommentForm.tsx
+++ client/src/components/movies/forms/CommentForm.tsx
@@ -8,20 +8,18 @@ interface FormData {
 
 export const CommentForm: React.FC = () => {
   const { register, handleSubmit, errors } = useForm<FormData>();
+  const movieId = localStorage.getItem("movie_id");
+  const authToken = localStorage.getItem("auth-token") as string;
 
   const onSubmit = handleSubmit(({ comment, rating }) => {
-    console.log("submit form");
-    console.log(comment);
-    console.log(rating);
-
     fetch("/api/ratings", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "auth-token": localStorage.getItem("auth-token") as string,
+        "auth-token": authToken,
       },
       body: JSON.stringify({
-        movieId: localStorage.getItem("movie_id"),
+        movieId: movieId,
         comment: comment,
         rating: +rating,
       }),
@@ -37,16 +35,23 @@ export const CommentForm: React.FC = () => {
       <textarea
         placeholder="Leave a comment"
         name="comment"
-        ref={register({ required: true })}
+        ref={register({ required: true, minLength: 3, maxLength: 2055 })}
       />
       {errors.comment && errors.comment.type === "required" && (
         <span>Leaving a comment is required</span>
+      )}
+      {errors.comment && errors.comment.type === "minLength" && (
+        <span>Comment must be at least 3 characters long</span>
+      )}
+      {errors.comment && errors.comment.type === "maxLength" && (
+        <span>Comment must be less than 2055 characters long</span>
       )}
       <br />
       <input
         type="number"
         step=".01"
         min="0"
+        max="10"
         placeholder="Leave a rating"
         name="rating"
         ref={register({ required: true })}

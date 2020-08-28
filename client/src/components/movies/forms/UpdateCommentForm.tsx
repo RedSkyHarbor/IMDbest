c5dacc032e1d989db92340002e393cf95a79e23c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-28 14:49:32 -0400

    basic styling for update comment form

--- client/src/components/movies/forms/UpdateCommentForm.tsx
+++ client/src/components/movies/forms/UpdateCommentForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import { Text, Input, Button, Textarea } from "@chakra-ui/core";
 
 interface FormData {
   comment: string;
@@ -42,23 +43,25 @@ export const UpdateCommentForm: React.FC<FormData> = (props) => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h4>Update your review</h4>
-      <textarea
+      <Text mt="4" fontSize="xl">
+        Update your review
+      </Text>
+      <Textarea
         defaultValue={props.comment}
         name="comment"
         ref={register({ required: true, minLength: 3, maxLength: 2055 })}
       />
       {errors.comment && errors.comment.type === "required" && (
-        <span>Leaving a comment is required</span>
+        <Text>Leaving a comment is required</Text>
       )}
       {errors.comment && errors.comment.type === "minLength" && (
-        <span>Comment must be at least 3 characters long</span>
+        <Text>Comment must be at least 3 characters long</Text>
       )}
       {errors.comment && errors.comment.type === "maxLength" && (
-        <span>Comment must be less than 2055 characters long</span>
+        <Text>Comment must be less than 2055 characters long</Text>
       )}
       <br />
-      <input
+      <Input
         defaultValue={props.rating}
         type="number"
         step=".01"
@@ -68,10 +71,9 @@ export const UpdateCommentForm: React.FC<FormData> = (props) => {
         ref={register({ required: true })}
       />
       {errors.rating && errors.rating.type === "required" && (
-        <span>Leaving a rating is required</span>
+        <Text>Leaving a rating is required</Text>
       )}
-      <br />
-      <input type="submit" />
+      <Button type="submit">Submit</Button>
     </form>
   );
 };

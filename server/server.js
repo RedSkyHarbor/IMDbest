68b341b26b683ac4edd0270880e21479e162eb9c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-31 12:27:20 -0400

    testing with cors enabled for all origins

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ const sessions = require("./routes/sessions");
 const ratings = require("./routes/ratings");
 
 // Configure cors to only accept requests from our client
-const corsOptions = {
-  origin: "https://imdbest.herokuapp.com/",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const origin = new RegExp("https://imdbest.herokuapp.com/(.*)");
+// const corsOptions = {
+//   origin: "https://imdbest.herokuapp.com/",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 const port = process.env.PORT || 5000;
 const app = express();

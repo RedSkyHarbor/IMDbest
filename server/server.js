327c4c0004ee957061d6d0396bbaed37a78f8765
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-30 20:12:46 -0400

    add cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 // Require routes
 const index = require("./routes/index");
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));

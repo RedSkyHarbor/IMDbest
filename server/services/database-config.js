5299a63ea22dbeac53b66f4a329fd0b9cd9eef25
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-08-30 20:06:46 -0400

    testing deployment

--- server/services/database-config.js
+++ server/services/database-config.js
@@ -7,7 +7,8 @@ const pool = new Pool({
   connectionString: isProduction
     ? process.env.DATABASE_URL
     : process.env.LOCAL_DATABASE_URL,
-  ssl: false,
+  //ssl: false,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = { pool };

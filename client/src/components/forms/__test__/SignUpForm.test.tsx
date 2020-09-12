4430b1194cca4ff9cffefb8e1f2f8e3f1a3236a5
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-09-12 16:01:28 -0400

    test that errors show when user submits empty sign up form

--- client/src/components/forms/__test__/SignUpForm.test.tsx
+++ client/src/components/forms/__test__/SignUpForm.test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { renderWithTheme } from "../../../setupTestUtils";
 import { SignUpForm } from "../SignUpForm";
+import { screen, fireEvent } from "@testing-library/react";
+
+const mockLogin = jest.fn((email, password) => {
+  return Promise.resolve({ email, password });
+});
 
 describe("<SignUpForm />", () => {
-  it("smoke test", () => {
+  beforeEach(() => {
     renderWithTheme(<SignUpForm />);
+  });
+
+  it("should display required error when inputs are empty", async () => {
+    fireEvent.submit(screen.getByRole("button"));
+    expect(await screen.findAllByRole("alert")).toHaveLength(3);
+    expect(mockLogin).not.toBeCalled();
   });
 });

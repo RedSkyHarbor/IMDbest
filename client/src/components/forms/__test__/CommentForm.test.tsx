f8a37fdc6195b2863f39e3f89dacd6e82132a9ed
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-09-13 14:43:32 -0400

    test commentform to show errors when no input provided

--- client/src/components/forms/__test__/CommentForm.test.tsx
+++ client/src/components/forms/__test__/CommentForm.test.tsx
@@ -3,6 +3,10 @@ import { CommentForm } from "../CommentForm";
 import { renderWithTheme } from "../../../setupTestUtils";
 import { screen, fireEvent } from "@testing-library/react";
 
+const mockComment = jest.fn((comment, rating, headline) => {
+  return Promise.resolve({ comment, rating, headline });
+});
+
 describe("<CommentForm />", () => {
   beforeEach(() => {
     renderWithTheme(<CommentForm movie_id="1" />);
@@ -11,5 +15,6 @@ describe("<CommentForm />", () => {
   it("should display required error when inputs are empty", async () => {
     fireEvent.submit(screen.getByRole("button"));
     expect(await screen.findAllByRole("alert")).toHaveLength(3);
+    expect(mockComment).not.toBeCalled();
   });
 });
